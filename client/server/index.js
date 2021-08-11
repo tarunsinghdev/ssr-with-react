05805d365587872501083dc92fa024aef07bd1ff
Tarunsingh56 <tarunsingh5169202@gmail.com>
2021-08-11 16:22:00 +0530

    Code cleanup :gun:

--- client/server/index.js
+++ client/server/index.js
@@ -1,14 +1,8 @@
-// import 'ignore-styles';
 require('ignore-styles');
+
 require('@babel/register')({
   ignore: [/node_modules/],
   presets: ['@babel/preset-env', '@babel/preset-react'],
 });
 
-// import babelRegister from '@babel/register';
-// babelRegister({
-//   ignore: [/node_modules/],
-//   presets: ['@babel/preset-env', '@babel/preset-react'],
-// });
-// import './server.js';
 require('./server');
